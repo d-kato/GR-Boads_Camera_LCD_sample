17be7779e103db4b9ad44b0d2402dd50000921c5
Daiki Kato <daiki.kato.cj@renesas.com>
2017-04-20 17:49:36 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,64 @@
+# GR-Boads_Camera_LCD_sample
+GR-PEACH、および、GR-LYCHEEで動作するサンプルプログラムです。  
+GR-LYCHEEの開発環境については、[GR-LYCHEE用オフライン開発環境の手順](https://developer.mbed.org/users/dkato/notebook/offline-development-lychee-langja/)を参照ください。
+
+
+## 概要
+カメラ画像をLCD、または、Windows用PCアプリ**DisplayApp**に表示させるサンプルです。  
+
+### カメラとLCDの設定
+``mbed_app.json``ファイルを変更することでLCD表示をONにできます。
+```json
+{
+    "config": {
+        "camera":{
+            "help": "0:disable 1:enable",
+            "value": "1"
+        },
+        "lcd":{
+            "help": "0:disable 1:enable",
+            "value": "1"
+        }
+    }
+}
+```
+
+カメラとLCDの指定を行う場合は``mbed_app.json``に以下を追加してください。
+```json
+{
+    "config": {
+        "camera":{
+            "help": "0:disable 1:enable",
+            "value": "1"
+        },
+        "camera-type":{
+            "help": "Options are CAMERA_CVBS, CAMERA_MT9V111, CAMERA_OV7725",
+            "value": "CAMERA_CVBS"
+        },
+        "lcd":{
+            "help": "0:disable 1:enable",
+            "value": "1"
+        },
+        "lcd-type":{
+            "help": "Options are GR_PEACH_4_3INCH_SHIELD, GR_PEACH_7_1INCH_SHIELD, GR_PEACH_RSK_TFT, GR_PEACH_DISPLAY_SHIELD, GR_LYCHEE_LCD",
+            "value": "GR_PEACH_4_3INCH_SHIELD"
+        }
+    }
+}
+```
+camera-typeとlcd-typeを指定しない場合は以下の設定となります。  
+* GR-PEACH、カメラ：CAMERA_MT9V111、LCD：GR_PEACH_4_3INCH_SHIELD  
+* GR-LYCHEE、カメラ：CAMERA_OV7725、LCD：GR_LYCHEE_LCD  
+
+
+### Windows用PCアプリで表示する
+``main.cpp``の``JPEG_SEND``に``1``を設定すると、カメラ画像をPCアプリに表示する機能が有効になります。  
+カメラ画像はJPEGに変換され、USBファンクションのCDCクラス通信でPCに送信します。  
+```cpp
+/**** User Selection *********/
+/** JPEG out setting **/
+#define JPEG_SEND              (1)                 /* Select  0(JPEG images are not output to PC) or 1(JPEG images are output to PC on USB(CDC) for focusing the camera) */
+/*****************************/
+```
+PC用アプリは以下よりダウンロードできます。  
+[DisplayApp](https://developer.mbed.org/users/dkato/code/DisplayApp/)  
---
+++ main.cpp
@@ -0,0 +1,167 @@
+
+#include "mbed.h"
+#include "EasyAttach_CameraAndLCD.h"
+
+/**** User Selection *********/
+/** JPEG out setting **/
+#define JPEG_SEND              (1)                 /* Select  0(JPEG images are not output to PC) or 1(JPEG images are output to PC on USB(CDC) for focusing the camera) */
+/*****************************/
+
+/* Video input and LCD layer 0 output */
+#define VIDEO_FORMAT           (DisplayBase::VIDEO_FORMAT_YCBCR422)
+#define GRAPHICS_FORMAT        (DisplayBase::GRAPHICS_FORMAT_YCBCR422)
+#define WR_RD_WRSWA            (DisplayBase::WR_RD_WRSWA_32_16BIT)
+#define DATA_SIZE_PER_PIC      (2u)
+
+/*! Frame buffer stride: Frame buffer stride should be set to a multiple of 32 or 128
+    in accordance with the frame buffer burst transfer mode. */
+#if MBED_CONF_APP_LCD
+  #define VIDEO_PIXEL_HW       LCD_PIXEL_WIDTH   /* QVGA */
+  #define VIDEO_PIXEL_VW       LCD_PIXEL_HEIGHT  /* QVGA */
+#else
+  #define VIDEO_PIXEL_HW       (640u)  /* VGA */
+  #define VIDEO_PIXEL_VW       (480u)  /* VGA */
+#endif
+
+#define FRAME_BUFFER_STRIDE    (((VIDEO_PIXEL_HW * DATA_SIZE_PER_PIC) + 31u) & ~31u)
+#define FRAME_BUFFER_HEIGHT    (VIDEO_PIXEL_VW)
+
+DisplayBase Display;
+
+#if defined(__ICCARM__)
+#pragma data_alignment=32
+static uint8_t user_frame_buffer0[FRAME_BUFFER_STRIDE * FRAME_BUFFER_HEIGHT]@ ".mirrorram";
+#else
+static uint8_t user_frame_buffer0[FRAME_BUFFER_STRIDE * FRAME_BUFFER_HEIGHT]__attribute((section("NC_BSS"),aligned(32)));
+#endif
+
+#if JPEG_SEND
+#include "JPEG_Converter.h"
+#include "DisplayApp.h"
+#include "dcache-control.h"
+
+#if defined(__ICCARM__)
+#pragma data_alignment=32
+static uint8_t JpegBuffer[2][1024 * 63];
+#else
+static uint8_t JpegBuffer[2][1024 * 63]__attribute((aligned(32)));
+#endif
+static size_t jcu_encode_size[2];
+static JPEG_Converter Jcu;
+static int jcu_buf_index_write = 0;
+static int jcu_buf_index_write_done = 0;
+static int jcu_buf_index_read = 0;
+static volatile int jcu_encoding = 0;
+static volatile int image_change = 0;
+static DisplayApp  display_app;
+
+static void JcuEncodeCallBackFunc(JPEG_Converter::jpeg_conv_error_t err_code) {
+    jcu_buf_index_write_done = jcu_buf_index_write;
+    image_change = 1;
+    jcu_encoding = 0;
+}
+
+static void snapshot(void) {
+    while ((jcu_encoding == 1) || (image_change == 0)) {
+        Thread::wait(1);
+    }
+    jcu_buf_index_read = jcu_buf_index_write_done;
+    image_change = 0;
+
+    display_app.SendJpeg(JpegBuffer[jcu_buf_index_read], (int)jcu_encode_size[jcu_buf_index_read]);
+}
+
+static void IntCallbackFunc_Vfield(DisplayBase::int_type_t int_type) {
+    //Interrupt callback function
+    if (jcu_encoding == 0) {
+        JPEG_Converter::bitmap_buff_info_t bitmap_buff_info;
+        JPEG_Converter::encode_options_t   encode_options;
+
+        bitmap_buff_info.width              = VIDEO_PIXEL_HW;
+        bitmap_buff_info.height             = VIDEO_PIXEL_VW;
+        bitmap_buff_info.format             = JPEG_Converter::WR_RD_YCbCr422;
+        bitmap_buff_info.buffer_address     = (void *)user_frame_buffer0;
+
+        encode_options.encode_buff_size     = sizeof(JpegBuffer[0]);
+        encode_options.p_EncodeCallBackFunc = &JcuEncodeCallBackFunc;
+        encode_options.input_swapsetting    = JPEG_Converter::WR_RD_WRSWA_32_16_8BIT;
+
+        jcu_encoding = 1;
+        if (jcu_buf_index_read == jcu_buf_index_write) {
+            jcu_buf_index_write ^= 1;  // toggle
+        }
+        jcu_encode_size[jcu_buf_index_write] = 0;
+        dcache_invalid(JpegBuffer[jcu_buf_index_write], sizeof(JpegBuffer[0]));
+        if (Jcu.encode(&bitmap_buff_info, JpegBuffer[jcu_buf_index_write],
+            &jcu_encode_size[jcu_buf_index_write], &encode_options) != JPEG_Converter::JPEG_CONV_OK) {
+            jcu_encode_size[jcu_buf_index_write] = 0;
+            jcu_encoding = 0;
+        }
+    }
+}
+#endif
+
+static void Start_Video_Camera(void) {
+    // Video capture setting (progressive form fixed)
+    Display.Video_Write_Setting(
+        DisplayBase::VIDEO_INPUT_CHANNEL_0,
+        DisplayBase::COL_SYS_NTSC_358,
+        (void *)user_frame_buffer0,
+        FRAME_BUFFER_STRIDE,
+        VIDEO_FORMAT,
+        WR_RD_WRSWA,
+        VIDEO_PIXEL_VW,
+        VIDEO_PIXEL_HW
+    );
+    EasyAttach_CameraStart(Display, DisplayBase::VIDEO_INPUT_CHANNEL_0);
+}
+
+#if MBED_CONF_APP_LCD
+static void Start_LCD_Display(void) {
+    DisplayBase::rect_t rect;
+
+    rect.vs = 0;
+    rect.vw = LCD_PIXEL_HEIGHT;
+    rect.hs = 0;
+    rect.hw = LCD_PIXEL_WIDTH;
+    Display.Graphics_Read_Setting(
+        DisplayBase::GRAPHICS_LAYER_0,
+        (void *)user_frame_buffer0,
+        FRAME_BUFFER_STRIDE,
+        GRAPHICS_FORMAT,
+        WR_RD_WRSWA,
+        &rect
+    );
+    Display.Graphics_Start(DisplayBase::GRAPHICS_LAYER_0);
+
+    Thread::wait(50);
+    EasyAttach_LcdBacklight(true);
+}
+#endif
+
+int main(void) {
+    // Initialize the background to black
+    for (int i = 0; i < sizeof(user_frame_buffer0); i += 2) {
+        user_frame_buffer0[i + 0] = 0x10;
+        user_frame_buffer0[i + 1] = 0x80;
+    }
+
+    EasyAttach_Init(Display);
+#if JPEG_SEND
+    // Interrupt callback function setting (Field end signal for recording function in scaler 0)
+    Display.Graphics_Irq_Handler_Set(DisplayBase::INT_TYPE_S0_VFIELD, 0, IntCallbackFunc_Vfield);
+#endif
+    Start_Video_Camera();
+#if MBED_CONF_APP_LCD
+    Start_LCD_Display();
+#endif
+
+    while (1) {
+#if JPEG_SEND
+        snapshot();
+#else
+        Thread::wait(1000);
+#endif
+    }
+}
+
